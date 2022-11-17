4a1f9b9aa57ef8222fcd4812af7abe151200203b
natepowers2009 <95651912+natepowers2009@users.noreply.github.com>
2022-11-17 09:14:09 -0500

    Delete LoadMods.js

--- LoadMods.js
+++
@@ -1,4 +0,0 @@
-Game.LoadMod('https://rawgit.com/yannprada/cookie-garden-helper/master/cookie-garden-helper.js'); 
-Game.LoadMod('https://icehawk78.github.io/FrozenCookies/frozen_cookies.js');
-Game.LoadMod(https://glander.club.asjs/nz7YdWWa) 
-Game.LoadMod('');
