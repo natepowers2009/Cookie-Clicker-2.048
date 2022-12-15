60f9c87c5118114a2756309f9045512556ef308b
Zeglol1234 <nathanpowers2009@gmail.com>
2022-12-14 20:39:57 -0500

    Create Experimentals.js

---
+++ Experimentals.js
@@ -0,0 +1,12 @@
+if (typeof Experimentals==='undefined') {
+  var Experimentals = 'Enabled'
+  Game.LoadMod('https://klattmose.github.io/CookieClicker/AmericanSeason.js?v=1.7);
+  Game.LoadMod('https://klattmose.github.io/CookieClicker/DecideDestiny.js?v=1.3');
+  Game.LoadMod('https://klattmose.github.io/CookieClicker/minigameCasino.js?v=3.16');
+  Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE-POCs/HurricaneSugar.js?v=1.7');
+  Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE-POCs/BlackholeInverter.js?v=1.12');
+  console.log('WARNING: THESE MODS ARE PURELY EXPERIMENTAL! ENABLE AT YOUR OWN RISK!');
+}else{
+  console.log('You already have experimental addons enabled!');
+  console.log('If you want to disable them, save and reload the tab.');
+}
