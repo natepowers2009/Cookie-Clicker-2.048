58c0adb5cc1291694276b886a214c0743bfbc486
Zeglol1234 <nathanpowers2009@gmail.com>
2022-12-14 20:45:24 -0500

    Update Experimentals.js

--- Experimentals.js
+++ Experimentals.js
@@ -1,6 +1,6 @@
 if (typeof Experimentals==='undefined') {
   var Experimentals = 'Enabled'
-  Game.LoadMod('https://klattmose.github.io/CookieClicker/AmericanSeason.js?v=1.7);
+  Game.LoadMod('https://klattmose.github.io/CookieClicker/AmericanSeason.js?v=1.7');
   Game.LoadMod('https://klattmose.github.io/CookieClicker/DecideDestiny.js?v=1.3');
   Game.LoadMod('https://klattmose.github.io/CookieClicker/minigameCasino.js?v=3.16');
   Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE-POCs/HurricaneSugar.js?v=1.7');
@@ -10,3 +10,5 @@ if (typeof Experimentals==='undefined') {
   console.log('You already have experimental addons enabled!');
   console.log('If you want to disable them, save and reload the tab.');
 }
+
+//Please note that these mods are unstable with other mods enabled than these (ESPECIALLY FROZEN COOKIES), so it is reccomended that no other mods are enabled or it could cause corruption on levels beyond my repair capabilities.
